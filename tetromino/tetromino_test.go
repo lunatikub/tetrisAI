44c85c86520653b50216eb929b9d7d726ee28aae
Thomas Joly <joly.th@gmail.com>
2020-09-07 15:46:57 +0200

    Tetromino I

--- tetromino/tetromino_test.go
+++ tetromino/tetromino_test.go
@@ -5,11 +5,11 @@ import (
 )
 
 func (t *tetromino) TestTetromino(test *testing.T) {
-	for _, p := range t.pieces {
+	for r, p := range t.pieces {
 
 		if p.width != len(p.cells[0]) || p.height != len(p.cells) {
-			test.Errorf("tetromino %s: dims expected {h:%d, w:%d}, got: {h:%d, w:%d}",
-				t.name, p.height, p.width, len(p.cells), len(p.cells[0]))
+			test.Errorf("tetromino [%s,%d]: dims expected {h:%d, w:%d}, got: {h:%d, w:%d}",
+				t.name, r, p.height, p.width, len(p.cells), len(p.cells[0]))
 			return
 		}
 
@@ -24,13 +24,30 @@ func (t *tetromino) TestTetromino(test *testing.T) {
 		}
 		for i, v := range p.heightCol {
 			if v != hc[i] {
-				test.Errorf("tetromino %s: heightCol[%d] expected:%d, got:%d",
-					t.name, i, v, hc[i])
+				test.Errorf("tetromino [%s,%d]: heightCol[%d] expected:%d, got:%d",
+					t.name, r, i, v, hc[i])
+				return
 			}
+		}
+
+		n := 0
+		for y, row := range p.cells {
+			for x := range row {
+				if p.isFilled(y, x) {
+					n++
+				}
+			}
+		}
+		if n != 4 {
+			test.Errorf("tetromino [%s,%d]: expected 4 cells, got: %d", t.name, r, n)
 		}
 	}
 }
 
 func TestTetrominoZ(test *testing.T) {
 	tetrominoZ.TestTetromino(test)
+}
+
+func TestTetrominoI(test *testing.T) {
+	tetrominoI.TestTetromino(test)
 }
